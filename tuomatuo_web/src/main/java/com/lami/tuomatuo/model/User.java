b3a371ca91b7a008785667b04102e411f700b122
xjk <xujiankang@alphaun.com>
2015-10-26 17:26:38 +0800

    init mudle

--- tuomatuo_web/src/main/java/com/lami/tuomatuo/model/User.java
+++ tuomatuo_web/src/main/java/com/lami/tuomatuo/model/User.java
@@ -5,14 +5,8 @@ package com.lami.tuomatuo.model;
  */
 
 
+import javax.persistence.*;
 import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "user")
