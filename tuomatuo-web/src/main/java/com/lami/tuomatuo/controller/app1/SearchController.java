6b37534b3d2036773836df0e520777ae3fc9b4cf
xujiankang <xujiankang.sh@superjia.com>
2016-01-18 13:57:23 +0800

    add controller

---
+++ tuomatuo-web/src/main/java/com/lami/tuomatuo/controller/app1/SearchController.java
@@ -0,0 +1,18 @@
+package com.lami.tuomatuo.controller.app1;
+
+import com.lami.tuomatuo.controller.BaseController;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by xujiankang on 2016/1/18.
+ */
+@Controller
+public class SearchController extends BaseController {
+
+    @Override
+    protected boolean checkAuth() {
+        return false;
+    }
+
+
+}
