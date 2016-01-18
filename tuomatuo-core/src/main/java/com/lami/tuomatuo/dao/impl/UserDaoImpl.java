f71b9996aa83bf971bb7c0a43008d8c7b25d431b
xujiankang <xujiankang.sh@superjia.com>
2016-01-18 19:04:13 +0800

    aa

--- tuomatuo-core/src/main/java/com/lami/tuomatuo/dao/impl/UserDaoImpl.java
+++ tuomatuo-core/src/main/java/com/lami/tuomatuo/dao/impl/UserDaoImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
  * Created by xjk on 11/2/15.
  */
 @Repository("userDaoInterface")
-public class UserDaoImpl extends BaseDaoMysqlImpl<User, Integer> implements UserDaoInterface {
+public class UserDaoImpl extends BaseDaoMysqlImpl<User, Long> implements UserDaoInterface {
     public UserDaoImpl(){
         super(User.class);
     }
