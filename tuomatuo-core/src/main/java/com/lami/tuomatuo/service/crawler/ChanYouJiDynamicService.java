9fd5bca3ca6842a7e4f76f4dacad317d7b929015
xujiankang <xujiankang.sh@superjia.com>
2016-03-24 17:41:08 +0800

    add @service

--- tuomatuo-core/src/main/java/com/lami/tuomatuo/service/crawler/ChanYouJiDynamicService.java
+++ tuomatuo-core/src/main/java/com/lami/tuomatuo/service/crawler/ChanYouJiDynamicService.java
@@ -6,10 +6,12 @@ import com.lami.tuomatuo.dao.crawler.ChanYoujiDynamicDaoInterface;
 import com.lami.tuomatuo.model.crawler.ChanYouJiAccount;
 import com.lami.tuomatuo.model.crawler.ChanYoujiDynamic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by xujiankang on 2016/3/24.
  */
+@Service("chanYouJiDynamicService")
 public class ChanYouJiDynamicService extends BaseService<ChanYoujiDynamic, Long> {
 
     @Autowired
