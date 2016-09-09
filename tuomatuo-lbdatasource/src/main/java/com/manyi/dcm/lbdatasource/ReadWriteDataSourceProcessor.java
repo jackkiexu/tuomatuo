2a8b5126c326ade8901f85b87eba820be365e7f6
xujiankang <xujiankang.sh@superjia.com>
2016-09-09 20:00:33 +0800

    modify some code

--- tuomatuo-lbdatasource/src/main/java/com/manyi/dcm/lbdatasource/ReadWriteDataSourceProcessor.java
+++ tuomatuo-lbdatasource/src/main/java/com/manyi/dcm/lbdatasource/ReadWriteDataSourceProcessor.java
@@ -5,7 +5,9 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.NestedRuntimeException;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
+import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
 import org.springframework.transaction.interceptor.TransactionAttribute;
 import org.springframework.util.PatternMatchUtils;
 import org.springframework.util.ReflectionUtils;
@@ -44,13 +46,26 @@ public class ReadWriteDataSourceProcessor implements BeanPostProcessor {
             Map<String, TransactionAttribute> nameMap = (Map<String, TransactionAttribute>) nameMapField.get(transactionAttributeSource);
 
             for(Map.Entry<String, TransactionAttribute> entry : nameMap.entrySet()){
-
+                RuleBasedTransactionAttribute attr = (RuleBasedTransactionAttribute)entry.getValue();
+                if(!attr.isReadOnly()){
+                    continue;
+                }
+                String methodname = entry.getKey();
+                Boolean isForceChoiceRead = Boolean.FALSE;
+                if(forceChoiceReadWhenWrite){
+                    attr.setPropagationBehavior(Propagation.NOT_SUPPORTED.value());
+                    isForceChoiceRead = Boolean.FALSE;
+                }else{
+                    attr.setPropagationBehavior(Propagation.SUPPORTS.value());
+                }
+                logger.debug("read/write transaction process method :" + methodname + ", force read : " + isForceChoiceRead);
+                readMethodMap.put(methodname, isForceChoiceRead);
             }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
 
-        return null;
+        return bean;
     }
 
     public Object determineReadOrWriteDB(ProceedingJoinPoint pjp)throws Throwable{
