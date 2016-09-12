50d8f541ccf35ecdf4f64678836acb1070ef8e20
xujiankang <xujiankang.sh@superjia.com>
2016-09-12 19:55:07 +0800

    add new module

---
+++ tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/redis/Constants.java
@@ -0,0 +1,15 @@
+package com.lami.tuomatuo.mq.redis;
+
+/**
+ * Created by xujiankang on 2016/9/12.
+ */
+public class Constants {
+
+    public static final String host = "127.0.0.1";
+    public static final int port = 6379;
+
+    public static final String SUBSCRIBE_CENTER = "_-subscribe-center-_";
+
+    public static final String MESSAGE_TXID = "_-message-txid-_";
+
+}
---
+++ tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/redis/PPubSubTestMain.java
@@ -0,0 +1,39 @@
+package com.lami.tuomatuo.mq.redis;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+/**
+ * Created by xujiankang on 2016/9/12.
+ */
+public class PPubSubTestMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        PPubClient pubClient = new PPubClient(Constants.host, Constants.port);
+        final String channel = "pubsub-channel-p";
+        final PSubClient subClient = new PSubClient(Constants.host, Constants.port,"subClient-1");
+        Thread subThread = new Thread(new Runnable() {
+
+            public void run() {
+                System.out.println("----------subscribe operation begin-------");
+                //在API级别，此处为轮询操作，直到unsubscribe调用，才会返回
+                subClient.sub(channel);
+                System.out.println("----------subscribe operation end-------");
+
+            }
+        });
+        subThread.setDaemon(true);
+        subThread.start();
+        int i = 0;
+        while(i < 2){
+            String message = RandomStringUtils.random(6, true, true);//apache-commons
+            pubClient.pub(channel, message);
+            i++;
+            Thread.sleep(1000);
+        }
+        subClient.unsubscribe(channel);
+    }
+
+}
