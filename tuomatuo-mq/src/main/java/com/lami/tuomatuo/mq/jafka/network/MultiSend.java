3d0b71fa5cf2e6521cb3793dc342bd8963ceebe0
xujiankang <xujiankang.sh@superjia.com>
2016-10-08 19:48:19 +0800

    add some file

--- tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/jafka/network/MultiSend.java
+++ tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/jafka/network/MultiSend.java
@@ -1,5 +1,7 @@
 package com.lami.tuomatuo.mq.jafka.network;
 
+import java.io.IOException;
+import java.nio.channels.GatheringByteChannel;
 import java.util.Iterator;
 import java.util.List;
 
@@ -38,6 +40,20 @@ public abstract class MultiSend<S extends Send> extends AbstractSend {
     }
 
     public boolean complete(){
+        if(current != null) return false;
+        if(totalWritten != expectedBytesToWrite){
+            logger.info("mismatch in sending bytes over socket; expected: " + expectedBytesToWrite + " actual: " + totalWritten);
+        }
+        return true;
+    }
 
+    public int writeTo(GatheringByteChannel channel) throws IOException {
+        expectIncomplete();
+        int written = current.writeTo(channel);
+        totalWritten += written;
+        if(current.complete()){ // move to the next element while current element is finished writting
+            current = iter.hasNext() ? iter.next() : null;
+        }
+        return written;
     }
 }
