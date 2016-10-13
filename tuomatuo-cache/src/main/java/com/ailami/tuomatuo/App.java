d93af0f6cdf23e7a676b144f5927c0eb54f18a36
xujiankang <xujiankang.sh@superjia.com>
2016-10-13 11:52:10 +0800

    add new module

---
+++ tuomatuo-cache/src/main/java/com/ailami/tuomatuo/App.java
@@ -0,0 +1,13 @@
+package com.ailami.tuomatuo;
+
+/**
+ * Hello world!
+    ddddddddddd *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
