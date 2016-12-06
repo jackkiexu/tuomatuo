e29c67a45edf11a14994501409114cde2081a7b2
xjk <jiankang.xu@ttpai.cn>
2016-12-06 18:31:46 +0800

    add some comment

--- tuomatuo-cache/src/main/java/com/lami/tuomatuo/cache/guava/AbstractLocalCache.java
+++ tuomatuo-cache/src/main/java/com/lami/tuomatuo/cache/guava/AbstractLocalCache.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * http://blog.csdn.net/clementad/article/details/46491701
  * Created by xjk on 2016/12/6.
  */
 @Data
