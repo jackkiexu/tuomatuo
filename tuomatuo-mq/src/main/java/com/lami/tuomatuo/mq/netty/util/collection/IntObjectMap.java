c6fb5677f6ff1b4e1af6f19143eab24df985eeb0
xjk <xujiankang@alphaun.com>
2016-12-14 23:17:30 +0800

    add some assign tools

---
+++ tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/netty/util/collection/IntObjectMap.java
@@ -0,0 +1,7 @@
+package com.lami.tuomatuo.mq.netty.util.collection;
+
+/**
+ * Created by xjk on 12/14/16.
+ */
+public interface IntObjectMap {
+}
---
+++ tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/netty/util/internal/PlatformDependent.java
@@ -0,0 +1,11 @@
+package com.lami.tuomatuo.mq.netty.util.internal;
+
+import org.jctools.queues.MpscChunkedArrayQueue;
+
+/**
+ * Created by xjk on 12/14/16.
+ */
+public class PlatformDependent {
+
+    
+}
