180ddbe58d025cb8649aa7a28c8eb98823b56bc4
xujiankang <1695220577@qq.com>
2017-01-03 19:00:01 +0800

    add some tree code

--- tuomatuo-search/src/main/java/com/lami/tuomatuo/search/base/concurrent/tree/rbtree/RBTree.java
+++ tuomatuo-search/src/main/java/com/lami/tuomatuo/search/base/concurrent/tree/rbtree/RBTree.java
@@ -3,12 +3,16 @@ package com.lami.tuomatuo.search.base.concurrent.tree.rbtree;
 import org.apache.regexp.RE;
 
 /**
+ * http://blog.csdn.net/v_july_v/article/details/6105630
  * http://blog.csdn.net/yxc135/article/details/7939671
  * http://www.cnblogs.com/skywang12345/p/3603935.html
  * http://www.cnblogs.com/skywang12345/p/3624343.html
  * http://www.cnblogs.com/skywang12345/p/3245399.html#aa2
  *
  * Java 版本的 红黑树
+ *
+ * 红黑树, 一种二叉查找树, 但在每个节点上增加一个存储位表示节点的颜色, 可以是Red或Black
+ * 通过对任何一条从根到叶子路径上各个节点着色方式的限制, 红黑树确保没有一条路径会比其他路径长出两倍, 因而接近平衡的(http://blog.csdn.net/v_july_v/article/details/6105630)
  *
  * Created by xjk on 1/2/17.
  */
