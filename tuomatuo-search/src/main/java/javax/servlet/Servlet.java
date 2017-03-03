5767e444862cab0f6a658fe3ff9874bf419eeb66
xujiankang <1695220577@qq.com>
2017-03-03 18:43:28 +0800

    add some code

--- tuomatuo-search/src/main/java/javax/servlet/Servlet.java
+++ tuomatuo-search/src/main/java/javax/servlet/Servlet.java
@@ -1,6 +1,34 @@
 package javax.servlet;
 
 /**
+ * Defines methods that all servlets must implement
+ *
+ * <p>
+ *     A servlet is a small Java program that runs within a web server. Servlets
+ *     receive and respond to requests from Web client, usually across HTTP, the
+ *     HyperText Transfer Protocol
+ * </p>
+ *
+ * <p>
+ *     To implement this interface, you can write a generic servlet that extends
+ *     GenericServlet or an HTTP servlet that extends
+ *     HttpServlet
+ * </p>
+ *
+ * <p>
+ *      This interface defines methods to initialize a servlet, to service requests,
+ *      and to remove a servlet from the server. These are known as life-cycle
+ *      methods and are called in the following sequence
+ * </p>
+ *
+ * <li>
+ *     The servlet is constructed, then initialized with the init
+ *     method
+ *     Any calls from clients to the service method are handled
+ *     The servlet is constr
+ * </li>
+ *
+ *
  *
  * Created by xujiankang on 2017/3/3.
  */
