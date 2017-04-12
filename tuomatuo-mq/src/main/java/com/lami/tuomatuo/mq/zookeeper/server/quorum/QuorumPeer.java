015ce3e8504889813840070b56c35a2dbc987556
xujiankang <1695220577@qq.com>
2017-04-12 18:33:56 +0800

    add some zookeeper code

--- tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/zookeeper/server/quorum/QuorumPeer.java
+++ tuomatuo-mq/src/main/java/com/lami/tuomatuo/mq/zookeeper/server/quorum/QuorumPeer.java
@@ -1,21 +1,23 @@
 package com.lami.tuomatuo.mq.zookeeper.server.quorum;
 
+import com.lami.tuomatuo.mq.zookeeper.common.AtomicFileOutputStream;
 import com.lami.tuomatuo.mq.zookeeper.server.ServerCnxnFactory;
 import com.lami.tuomatuo.mq.zookeeper.server.ZKDatabase;
+import com.lami.tuomatuo.mq.zookeeper.server.ZooKeeperServer;
 import com.lami.tuomatuo.mq.zookeeper.server.ZooKeeperThread;
 import com.lami.tuomatuo.mq.zookeeper.server.persistence.FileTxnSnapLog;
 import com.lami.tuomatuo.mq.zookeeper.server.quorum.flexible.QuorumMaj;
 import com.lami.tuomatuo.mq.zookeeper.server.quorum.flexible.QuorumVerifier;
+import com.lami.tuomatuo.mq.zookeeper.server.util.ZxidUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.util.Map;
+import java.util.*;
 
 /**
  * The class manages the quorum protocol. There are three states this server
@@ -44,13 +46,13 @@ public class QuorumPeer extends Thread implements QuorumStats.Provider{
 
     private static final Logger LOG = LoggerFactory.getLogger(QuorumPeer.class);
 
-    QuorumBean jmxQuorumBean;
-    LocalPeerBean jmxLocakPeerbean;
-    LeaderElectionBean jmxLeaderElectionBean;
-    QuorumCnxManager qcm;
+    public QuorumBean jmxQuorumBean;
+    public LocalPeerBean jmxLocakPeerbean;
+    public LeaderElectionBean jmxLeaderElectionBean;
+    public QuorumCnxManager qcm;
 
     /**
-     * ZKDatabase is a top level member of quorumpeer
+     * ZKDatabase is a top level member of quorum peer
      * which will be used in all the zookeeperservers
      * instantiated later. Also. it is created once on
      * bootup and only thrown away in case of a truncate
@@ -198,6 +200,12 @@ public class QuorumPeer extends Thread implements QuorumStats.Provider{
      */
     protected int syncLimit;
 
+    /**
+     * Enable/Disable sync request processor. This option is enabled
+     * by default and is to be used with observers
+     */
+    public boolean syncEnabled = true;
+
     // The current tick
     protected volatile int tick;
 
@@ -315,9 +323,10 @@ public class QuorumPeer extends Thread implements QuorumStats.Provider{
 
     public int electionType;
 
-    Election electionAlg;
 
-    ServerCnxnFactory cnxnFactory;
+    public Election electionAlg;
+
+    public ServerCnxnFactory cnxnFactory;
 
     private FileTxnSnapLog logFactory = null;
 
@@ -362,7 +371,7 @@ public class QuorumPeer extends Thread implements QuorumStats.Provider{
         }
     }
 
-    QuorumStats quorumStats(){
+    public QuorumStats quorumStats(){
         return quorumStats;
     }
 
@@ -374,23 +383,357 @@ public class QuorumPeer extends Thread implements QuorumStats.Provider{
         super.start();
     }
 
+
+    public void loadDataBase(){
+        File updating = new File(getTxnFactory().getSnapDir(),
+                UPDATING_EPOCH_FILENAME);
+        try{
+            zkDb.loadDataBase();
+
+            // load the epochs
+            long lastProcessedZxid = zkDb.getDataTree().lastProcessedZxid;
+            long epochOfZxid = ZxidUtils.getEpochFromZxid(lastProcessedZxid);
+            try{
+
+            }catch (Exception e){
+                currentEpoch = epochOfZxid;
+
+            }
+        }catch (IOException e){
+            throw new RuntimeException("Unable to run quorum server", e);
+        }
+    }
+
     public QuorumPeer(String name) {
         super(name);
     }
 
 
-    @Override
-    public String[] getQuorumPeers() {
-        return new String[0];
-    }
+    public Follower follower;
+    public Leader leader;
+    public Observer observer;
 
-    @Override
-    public String getServerState() {
+
+
+
+    synchronized public ZooKeeperServer getActiveServer(){
         return null;
     }
 
+    @Override
+    public void run() {
+    }
+
+    public void shutdown(){
+
+    }
+
+    public Map<Long, QuorumPeer.QuorumServer> getView(){
+        return Collections.unmodifiableMap(this.quorumPeers);
+    }
+
+    public Map<Long, QuorumPeer.QuorumServer> getVotingView(){
+        Map<Long, QuorumPeer.QuorumServer> ret = new HashMap<>();
+        Map<Long, QuorumPeer.QuorumServer> view = getView();
+        for(QuorumObserver quorumObserver : view.values()){
+            if(server.type == LearnerType.PARRTICIPANT){
+                ret.put(server.id, quorumObserver);
+            }
+        }
+
+        return ret;
+    }
+
+    public Map<Long, QuorumPeer.QuorumServer> getObservingView(){
+        Map<Long, QuorumPeer.QuorumServer> ret = new HashMap<>();
+        Map<Long, QuorumPeer.QuorumServer> view = getView();
+        for(QuorumObserver quorumObserver : view.values()){
+            if(server.type == LearnerType.OBSERVER){
+                ret.put(server.id, quorumObserver);
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Check if a node is in the current view. With static membership, the
+     * result of the check will never change; only when dynamic membership
+     * is introduced will this be more useful
+     */
+    public boolean viewContains(Long sid){
+        return this.quorumPeers.containsKey(sid);
+    }
+
+    // Only used by QuorumStats at the moment
+    public String[] getQuorumPeers(){
+        List<String> l = new ArrayList<>();
+        synchronized (this){
+            if(leader != null){
+                for(LearnerHandler fh : leader.getLearners()){
+                    if(fh.getSocket() != null){
+                        String s = fh.getSocket().getRemoteSocketAddress().toString();
+                        if(leader.isLearnerSynced(fh)){
+                            s += "*";
+                        }
+                        l.add(s);
+                    }
+                }
+            }else if(follower != null){
+                l.add(follower.sock.getRemoteSocketAddress().toString());
+            }
+        }
+
+        return l.toArray(new String[0]);
+    }
+
+
+
+    public String getServerState(){
+        switch (getPeerState()){
+            case LOOKING:
+                return QuorumStats.Provider.LOOKING_STATE;
+            case LEADING:
+                return QuorumStats.Provider.LEADING_STATE;
+            case FOLLOWING:
+                return QuorumStats.Provider.FOLLOWING_STATE;
+            case OBSERVING:
+                return QuorumStats.Provider.OBSERVING_STATE;
+        }
+        return QuorumStats.Provider.UNKNOWN_STATE;
+    }
+
+
+
+
+
+    public long getMyid(){
+        return myid;
+    }
+
+    public void setMyid(long myid){
+        this.myid = myid;
+    }
+
+    public int getTickTime(){
+        return tickTime;
+    }
+
+    public void setTickTime(int tickTime){
+        this.tickTime = tickTime;
+    }
+
+    public int getMaxClientCnxnPerHost(){
+        ServerCnxnFactory fac = getCnxnFactory();
+        if(fac == null){
+            return -1;
+        }
+        return fac.getMaxClientCnxnsPerHost();
+    }
+
+    public int getMaxSessionTimeout(){
+        return minSessionTimeout == -1? tickTime * 2 : minSessionTimeout;
+    }
+
+    public void setMaxSessionTimeout(int max){
+        this.maxSessionTimeout = max;
+    }
+
+    public int getInitLimit(){
+        return initLimit;
+    }
+
+    public void setInitLimit(int initLimit){
+        this.initLimit = initLimit;
+    }
+
+    public int getTick(){
+        return tick;
+    }
+
+    public QuorumVerifier getQuorumVerifier(){
+        return quorumConfig;
+    }
+
+    public void setQuorumVerifier(QuorumVerifier quorumConfig){
+        this.quorumConfig = quorumConfig;
+    }
+
+    public Election getElectionAlg(){
+        return electionAlg;
+    }
+
+    public int getSyncLimit(){
+        return syncLimit;
+    }
+
+    public void setSyncLimit(int syncLimit){
+        this.syncLimit = syncLimit;
+    }
+
+    // The syncEnabled can also be set via a system property
+    public static final String SYNC_ENABLED = "zookeeper.observer.syncEnabled";
+
+    public boolean getSyncEnabled(){
+        if(System.getProperty(SYNC_ENABLED) != null){
+            return Boolean.getBoolean(SYNC_ENABLED);
+        }else{
+            return syncEnabled;
+        }
+    }
+
+    public void setSyncEnabled(boolean syncEnabled){
+        this.syncEnabled = syncEnabled;
+    }
+
+    public int getElectionType(){
+        return electionType;
+    }
+
+    public void setElectionType(int electionType){
+        this.electionType = electionType;
+    }
+
+    public boolean getQuorumListenOnAllIPs(){
+        return quorumListenOnAllIPs;
+    }
 
     public void setQuorumListenOnAllIPs(boolean quorumListenOnAllIPs){
         this.quorumListenOnAllIPs = quorumListenOnAllIPs;
     }
+
+    public ServerCnxnFactory getCnxnFactory() { return cnxnFactory; }
+
+    public void setCnxnFactory(ServerCnxnFactory cnxnFactory){
+        this.cnxnFactory = cnxnFactory;
+    }
+
+    public void setQuorumPeers(Map<Long, QuorumServer> quorumPeers){
+        this.quorumPeers = quorumPeers;
+    }
+
+    public int getClientPort(){
+        return cnxnFactory.getLocalPort();
+    }
+
+    public void setClientPortAddress(InetSocketAddress addr){
+
+    }
+
+    public void setTxnFactory(FileTxnSnapLog factory){
+        this.logFactory = factory;
+    }
+
+    public FileTxnSnapLog getTxnFactory(){
+        return this.logFactory;
+    }
+
+    // set zk database for this node
+    public void setZKDatabase(ZKDatabase database){
+        this.zkDb = database;
+    }
+
+    public void setRunning(boolean running){
+        this.running = running;
+    }
+
+    public boolean isRunning(){
+        return running;
+    }
+
+
+    public QuorumCnxManager getQuorumCnxnManager() {
+        return qcm;
+    }
+
+    public long readLongFromFile(String name) throws Exception{
+        File file = new File(logFactory.getSnapDir(), name);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = "";
+        try{
+            line = br.readLine();
+            return Long.parseLong(line);
+        }catch (Exception e){
+            throw new IOException("Found " + line + " in" + file);
+        }
+        finally {
+            br.close();
+        }
+    }
+
+    public long acceptedEpoch = -1;
+    public long currentEpoch = -1;
+
+    public static final String CURRENT_EPOCH_FILENAME = "currentEpoch";
+    public static final String ACCEPTED_EPOCH_FILENAME = "acceptedEpoch";
+    public static final String UPDATING_EPOCH_FILENAME = "updatingEpoch";
+
+    public void writeLongToFile(String name, long value) throws Exception{
+        File file = new File(logFactory.getSnapDir(), name);
+        AtomicFileOutputStream out = new AtomicFileOutputStream(file);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+        boolean aborted = false;
+
+        try{
+            bw.write(Long.toString(value));
+            bw.flush();
+            out.flush();
+        }catch (IOException e){
+            LOG.info("Failed to write new file" + file , e);
+            // worst case here the tmp file/resources are not cleaned up
+            // and the caller will be notified
+            aborted = true;
+            out.abort();
+            throw e;
+        }finally {
+            if(!aborted){
+                // if the close operation (rename) fail we'll get notified
+                // woest case the tmp file may still exist
+                out.close();
+            }
+        }
+    }
+
+
+    public long getCurrentEpoch() throws Exception{
+        if(currentEpoch == -1){
+            currentEpoch = readLongFromFile(CURRENT_EPOCH_FILENAME);
+        }
+        return currentEpoch;
+    }
+
+    public long getAcceptedEpoch() throws Exception{
+        if(acceptedEpoch == -1){
+            acceptedEpoch = readLongFromFile(ACCEPTED_EPOCH_FILENAME);
+        }
+        return acceptedEpoch;
+    }
+
+    public void setCurrentEpoch(long e) throws Exception{
+        currentEpoch = e;
+        writeLongToFile(CURRENT_EPOCH_FILENAME, e);
+    }
+
+    public void setAcceptedEpoch(long e) throws Exception{
+        acceptedEpoch = e;
+        writeLongToFile(ACCEPTED_EPOCH_FILENAME, e);
+    }
+
+    /**
+     * Updates leader election info to avoid inconsistencies when
+     * a new server tries to join the ensemble
+     */
+    public void updateElectionVote(long newEpoch){
+        Vote currentVote = getCurrentVote();
+        setBCVote(currentVote);
+        if(currentVote != null){
+            setCurrentVote(new Vote(currentVote.getId(),
+                    currentVote.getZxid(),
+                    currentVote.getElectionEpoch(),
+                    newEpoch,
+                    currentVote.getState()));
+        }
+    }
+
+
 }
